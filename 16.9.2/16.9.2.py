f1c5f4f758f8cd0926a602fdd101799350828e53
Serg <113059310+gesarzx@users.noreply.github.com>
2023-03-12 02:48:00 +0300

    Add files via upload

---
+++ 16.9.2/16.9.2.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    def __init__(self, x, y, width, heigth):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.heigth = heigth
+
+    def __str__(self):
+        return f'Rectangle : {self.x}, {self.y}, {self.width}, {self.heigth}.'
+
+    def get_area(self):
+        return self.width * self.heigth
+
+
+rect_1 = Rectangle(5, 10, 50, 100)
+print(rect_1)
+print(rect_1.get_area())
